90c48920f5e0bfacf6ec309dd961b7a481e3d65b
Yudai_Ito <itoKey120@gmail.com>
2020-05-06 16:18:44 +0900

    確認

--- plugins/google/SpreadSheet.py
+++ plugins/google/SpreadSheet.py
@@ -3,11 +3,11 @@ from slackbot.bot import respond_to     # @botname: で反応するデコーダ
 from google import wks
 
 
-@respond_to('お寿司召喚')
-def read_sheet(message):
-  number_list = list(range(1, 1458))
-  import_value = wks.acell('A' + str(random.choice(number_list))).value
-  message.send(import_value) # メンション
+# @respond_to('お寿司召喚')
+# def read_sheet(message):
+#   number_list = list(range(1, 1458))
+#   import_value = wks.acell('A' + str(random.choice(number_list))).value
+#   message.send(import_value) # メンション
 
 @respond_to('お寿司大放出')
 def read_sheet(message):
